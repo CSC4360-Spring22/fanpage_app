27b28d779e8cb93cb81635b7e5e5c6c3c847db1d
daparikh2500 <daparikh25@gmail.com>
2022-02-14 17:48:28 -0500

    Initial home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
+import 'package:fanpage_app/Start.dart';
 import 'package:flutter/material.dart';
+import 'Start.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: Start(),
     );
   }
 }
